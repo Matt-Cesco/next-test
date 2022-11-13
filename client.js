9b66940dcdd298b8887101fe1d1046f3a1161f1c
Matt-Cesco <matteo.francesconi88@gmail.com>
2022-11-13 19:35:46 +0000

    speranza

--- client.js
+++ client.js
@@ -2,5 +2,8 @@ import sanityClient from '@sanity/client';
 
 export default sanityClient({
   projectId: "3x14gyw8",
-  dataset: "production"
+  dataset: "production",
+  apiVersion: "2021-08-31", // use a UTC date string
+  token: "sanity-auth-token", // or leave blank for unauthenticated usage
+  useCdn: true, // `false` if you want to ensure fresh data
 });
--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 
-
 module.exports = {
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.optimization.splitChunks.cacheGroups = {
+        ...config.optimization.splitChunks.cacheGroups,
+        "@sentry": {
+          test: /[\\/]node_modules[\\/](@sentry)[\\/]/,
+          name: "@sentry",
+          priority: 10,
+          reuseExistingChunk: false,
+        },
+      };
+    }
+
+    return config;
+  },
   images: {
     domains: ["cdn.sanity.io"],
   },
