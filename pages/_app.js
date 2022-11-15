56070e56ed2c263dbe1e07778ff241d82ed593e3
Matt-Cesco <matteo.francesconi88@gmail.com>
2022-11-15 21:49:53 +0000

    sto giro

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,16 @@
-import dynamic from "next/dynamic";
-import { Suspense } from "react";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
 
-const DynamicNavbar = dynamic(() => import("../components/navbar"), {
-  suspense: true,
-});
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Suspense fallback={`Loading...`}>
-        <DynamicNavbar />
+      
+        <Navbar />
         <Component {...pageProps} />
-      </Suspense>
+     
     </>
   );
 }
